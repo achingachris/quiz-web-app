92f6bd61c7799f22047f8bb6a9814c6d7c6b41d7
ChrisAchinga <achinga.chris@gmail.com>
2020-08-04 00:22:20 +0300

    custom

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,13 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>This is where I blog now!!</p>
+        <p>A Developer's Diary</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          All posts in this site are written by <a href="https://achinga.netlify.app/">Chris Achinga</a>
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Articles & Blog Posts</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
